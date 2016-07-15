b7720d87ab67099558524fc4d415e632a00295e7
SherlockCat <xi_afan@163.com>
2016-07-15 18:26:37 +0800

    更改rxjava的写法

--- app/src/main/java/com/xf/gankapp/presenter/IOSPresenter.java
+++ app/src/main/java/com/xf/gankapp/presenter/IOSPresenter.java
@@ -35,9 +35,57 @@ public class IOSPresenter implements IOSContract.Presenter {
 
     @Override
     public void subscribeIOSGank(int count, int page) {
-        Subscription subscription = mGankModule.getAndroid(count, page)
+        Subscription subscription = mGankModule.getIOS(count, page)
                 .subscribeOn(Schedulers.io())
-                .map(new Func1<AllResults, List<Gank>>() {
+                .flatMap(new Func1<AllResults, Observable<List<Gank>>>() {
+                    @Override
+                    public Observable<List<Gank>> call(final AllResults allResults) {
+                        if (!allResults.isError()) {
+                            return Observable.create(new Observable.OnSubscribe<List<Gank>>() {
+                                @Override
+                                public void call(Subscriber<? super List<Gank>> subscriber) {
+                                    subscriber.onNext(allResults.getGankList());
+                                    subscriber.onCompleted();
+                                }
+                            });
+                        }
+                        return Observable.error(new Exception("数据返回异常"));
+                    }
+                })
+                .flatMap(new Func1<List<Gank>, Observable<Gank>>() {
+                    @Override
+                    public Observable<Gank> call(List<Gank> ganks) {
+                        return Observable.from(ganks);
+                    }
+                })
+                .map(new Func1<Gank, Gank>() {
+                    @Override
+                    public Gank call(Gank gank) {
+                        String date = gank.getPublishedAt().substring(0, 10);
+                        gank.setPublishedAt(date);
+                        return gank;
+                    }
+                })
+                .toList()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Gank>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        mIOSView.showTip(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(List<Gank> ganks) {
+                        mIOSView.showIOSGank(ganks);
+                    }
+                });
+
+            /*    .map(new Func1<AllResults, List<Gank>>() {
 
                     @Override
                     public List<Gank> call(AllResults allResults) {
@@ -76,7 +124,7 @@ public class IOSPresenter implements IOSContract.Presenter {
                     public void onNext(List<Gank> ganks) {
                         mIOSView.showIOSGank(ganks);
                     }
-                });
+                });*/
         mSubscriptions.add(subscription);
     }
 
