27823922974224b71f355ddf19bba3253e17463b
SherlockCat <xi_afan@163.com>
2016-08-10 15:08:27 +0800

    修改不合理代码

--- app/src/main/java/com/xf/gankapp/module/GankModule.java
+++ app/src/main/java/com/xf/gankapp/module/GankModule.java
@@ -11,22 +11,24 @@ import rx.Observable;
  * Created by X-FAN on 2016/6/21.
  */
 public class GankModule implements IGankModule {
+    private GankService mGankService;
+
+    public GankModule() {
+        mGankService = RetrofitUtil.getRetrofit().create(GankService.class);
+    }
 
     @Override
     public Observable<AllResults> getAll(int count, int page) {
-        GankService gankService = RetrofitUtil.getRetrofit().create(GankService.class);
-        return gankService.getAll(count, page);
+        return mGankService.getAll(count, page);
     }
 
     @Override
     public Observable<AllResults> getAndroid(int count, int page) {
-        GankService gankService = RetrofitUtil.getRetrofit().create(GankService.class);
-        return gankService.getAndroid(count, page);
+        return mGankService.getAndroid(count, page);
     }
 
     @Override
     public Observable<AllResults> getIOS(int count, int page) {
-        GankService gankService = RetrofitUtil.getRetrofit().create(GankService.class);
-        return gankService.getIOS(count, page);
+        return mGankService.getIOS(count, page);
     }
 }
